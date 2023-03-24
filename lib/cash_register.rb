1f4f25da0545d7672a361f3ed2838e3fd8a704df
Rachel Hamby <tnthreat33@gmail.com>
2023-03-23 20:29:39 -0400

    commit tree f39bf92ec0ed9029551b7ab6e70e2e754ad4b8eb

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,8 @@
-
+class CashRegister
+    attr_accessor :total, :employee_discount
+  
+    def initialize(employee_discount=0)
+      @employee_discount = employee_discount
+      @total = 0
+    end
+  end
